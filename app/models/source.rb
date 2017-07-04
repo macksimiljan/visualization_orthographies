09801abfa889ce5440747164d6094729999f730c
macksimiljan <macksimiljan@users.noreply.github.com>
2017-07-04 16:00:51 +0200

    refactored sources

--- app/models/source.rb
+++ app/models/source.rb
@@ -2,4 +2,13 @@ class Source < ApplicationRecord
   has_many :attestations
   has_many :morpho_syntaxes, through: :attestations
   has_many :orthographies, through: :attestations
+
+  def typologies
+    [typo_broad, typo_content, typo_form,
+     typo_sociohistoric, typo_speccorpus, typo_subjectmatter].compact
+  end
+
+  def dating_interval
+    [dating_start, dating_end].compact
+  end
 end
